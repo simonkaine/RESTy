923fbed4f641f89ed377ee994ff6f18ffd184619
Simon Kaine <simonkaine@gmail.com>
2021-11-06 18:03:18 -0700

    Form buildout for URL + Radio btns & submit button

---
+++ src/components/app/container/RESTy.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from '../RESTy-app/Form';
+
+class RESTy extends React.Component {
+    state = {search: ''}
+
+    render() { 
+        return (
+            <Form />
+        );
+    }
+}
+ 
+export default RESTy;
