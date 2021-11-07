e13cf9777a8c39d765979b43a3d08113ef948ca3
Simon Kaine <simonkaine@gmail.com>
2021-11-06 18:27:44 -0700

    Response component rendering

--- src/components/app/container/RESTy.jsx
+++ src/components/app/container/RESTy.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Form from '../RESTy-app/Form';
+import Response from '../RESTy-app/Response';
 
 class RESTy extends React.Component {
     state = {search: ''}
 
     render() { 
         return (
+            <>
             <Form />
+            <Response />
+            </>
         );
     }
 }
